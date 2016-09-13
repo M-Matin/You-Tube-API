3d66b8fd02afafdb95e10b474ba76bb9489e91f9
Maha Matin <matin643@gmail.com>
2016-09-12 20:28:11 -0700

    Added jQuery Animation

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
-// Searchbar Handler
+
+// - - - - - - - - - - -animation for the search form- - - - - - - - - - - -
+//search bar Handler
 $(function(){
-	var searchField = $('#query');
-	var icon = $('#search-btn');
-	
-	// Focus Event Handler
-	$(searchField).on('focus', function(){
-		$(this).animate({
-			width:'100%'
-		},400);
-		$(icon).animate({
-			right: '10px'
-		}, 400);
-	});
-	
-	// Blur Event Handler
-	$(searchField).on('blur', function(){
-		if(searchField.val() == ''){
-			$(searchField).animate({
-				width:'45%'
-			},400, function(){});
-			$(icon).animate({
-				right:'360px'
-			},400, function(){});
-		}
-	});
-	
-	$('#search-form').submit(function(e){
-		e.preventDefault();
-	});
+  var searchField = $('#query');
+  var icon = $('#search-btn');
+
+  //focus event Handler to make search box animate when hover on it
+  $(searchField).on('focus', function(){ //run the function when searchfield is on focus
+    $(this).animate({ //this refer to searchfield box which needs to be animate
+        width:'100%'
+    },400);//speed fpr animate
+     $(icon).animate({//make search icon animate as well as searchField
+       right:'10px'
+      },400);
+  });
+    //blure event handler
+    $(searchField).on('blur', function(){
+      if(searchField.val() == ''){ //if searchField doesn't have any string make the animation
+        $(searchField).animate({ //animation for searchField
+          width:'45%'
+        },400);
+        $(icon).animate({ //animation for icon
+          right:'360px'
+        },400);
+
+      }
+    });
 });
+// - - - - - - - - -end of animation for search form - - - - - - - - - - - -
